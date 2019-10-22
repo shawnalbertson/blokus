a3a02c6b493ea71c23104b1e942ef789e825d36d
Shawn Albertson <shawn.albertson@students.olin.edu>
2019-10-21 22:01:29 -0400

    Further develop piece and board classes

--- model.py
+++ model.py
@@ -1,12 +1,36 @@
 # Function to draw a board with white tile pieces to start
+import pygame
 
+# Functions to load images
+def piece_size(board_size, n):
+    """Take board dimension tuple and number of desired pieces per board
+    Return the length of one edge of a tile as int type for pygame.transform.scale
+
+    A regular Blokus board is 20x20
+    """
+    return int(min(board_size)/n)
+
+def load_image(file_name, size):
+    """ Take file as filename
+    Take board size as length 2 tuple
+    Scale each image such that it fits as a square
+    """
+    # try:
+    #     im = pygame.image.load(file_name)
+
+    # except:
+    #     print("Cannot load image")
+    #     raise SystemExit
+    im = pygame.image.load(file_name)
+
+    return pygame.transform.scale(im, (size, size))
 
 class Board:
     """ The object that contains all tile information
+
+    base, c1, c2, c3, c4 are all tile images
     """
-
-
-    def __init__(self):
+    def __init__(self, screen, size, base, c1, c2, c3, c4):
         self.board = []
         self.row = []
         for m in range(20):
@@ -14,6 +38,14 @@ class Board:
                 self.row.append(".")
             self.board.append(self.row)
             self.row = []
+
+        self.screen = screen
+        self.size = size
+        self.base = base
+        self.c1 = c1
+        self.c2 = c2
+        self.c3 = c3
+        self.c4 = c4        
             
     def make_board(self):
         """Define board for optional input conditions. 
@@ -21,17 +53,45 @@ class Board:
 
         base tile = "."
         green tile = "g"
-        blue tile = "b"
         red tile = "r"
-        yellow tile = "y"
-        """
-        
+        yellow tile = "y"        
+        blue tile = "b"
+
+        """        
         for i1, m in enumerate(self.board):
             for i2, n in enumerate(m):
-                # print((i1, i2), n)
-                if n == "."
-                    screen.blit(white, (size*i1, size*i2))
+                if n == ".":
+                    self.screen.blit(self.base, (self.size*i1, self.size*i2))
+                elif n == "g":
+                    self.screen.blit(self.c1, (self.size*i1, self.size*i2))
+                elif n == "r":
+                    self.screen.blit(self.c2, (self.size*i1, self.size*i2))
+                elif n == "y":
+                    self.screen.blit(self.c3, (self.size*i1, self.size*i2))
+                elif n == "b":
+                    self.screen.blit(self.c2, (self.size*i1, self.size*i2))
 
 
-b = Board()
-print(b.make_board())
+class Piece:
+    """There will be an instance for every piece
+    Draw the shape in a 3x5 list of characters
+
+    the L5 piece might be:
+    [["x", "x", "x", ".", "."], ["x",".",".",".","."], ["x",".",".",".","."]]
+    """
+
+    def __init__(self, screen, size, color, array):
+        self.screen = screen
+        self.size = size
+        self.color = color       
+        self.array = array
+    
+  
+    def draw_piece(self):
+        (start_x, start_y) = pygame.mouse.get_pos()
+        for i1, m in enumerate(self.array):
+            for i2, n in enumerate(m):
+                if n == "x":
+                    self.screen.blit(self.color, (self.size*i1 + start_x, self.size*i2 + start_y))
+                else:
+                    continue
--- shawn.py
+++ shawn.py
@@ -1,6 +1,9 @@
 import pygame
 import time
 
+from model import Board, Piece, piece_size, load_image
+
+
 
 # Initialize pygame, screen object
 pygame.init()
@@ -15,30 +18,6 @@ pygame.display.set_caption('Blockus')
 background_color = (177,177,177)
 screen.fill(background_color)
 
-# Load images
-
-def piece_size(board_size, n):
-    """Take board dimension tuple and number of desired pieces per board
-    Return the length of one edge of a tile as int type for pygame.transform.scale
-
-    A regular Blokus board is 20x20
-    """
-    return int(min(board_size)/n)
-
-def load_image(file_name, size):
-    """ Take file as filename
-    Take board size as length 2 tuple
-    Scale each image such that it fits as a square
-    """
-    try:
-        im = pygame.image.load(file_name)
-
-    except:
-        print("Cannot load image")
-        raise SystemExit
-
-    return pygame.transform.scale(im, (size, size))
-
 # Define the size of one tile
 size = piece_size(board_size, 20)
 
@@ -49,33 +28,22 @@ yellow = load_image("yellow.png", size)
 blue = load_image("blue.png", size)
 white = load_image("white.png", size)
 
-# Function to draw a board with white tile pieces to start
-def draw_board():
-    for m in range(20):
-        for n in range(20):
-            screen.blit(white, (size*m, size*n))
+
+b = Board(screen, size, white, green, red, yellow, blue)
 
 # These few lines place an L5 piece in the top left corner
 # Next steps: more general
 #   make the blitting steps for every piece
 
-
-def L5(color, mod0, mod1):
-    """the mod inputs allow rotation, should be +/- 1"""
-    (start_x, start_y) = pygame.mouse.get_pos()
-
-    for m in [mod0 * i for i in range(3)]:
-        screen.blit(color, (size*m + start_x, 0 + start_y))
-
-    for m in [mod1 * i for i in range(3)]:
-        screen.blit(color, (0 + start_x, size*m + start_y))
+L5 = Piece(screen, size, red, [["x", "x", "x", ".", "."], ["x",".",".",".","."], ["x",".",".",".","."]])
 
 
 # Stop the code when the window is closed
 running = True
 while running:
-    draw_board()
-    L5(red, 1, 1)
+    b.make_board()
+    # L5(red, 1, 1)
+    L5.draw_piece()
     time.sleep(.01)
     pygame.display.flip()
     for event in pygame.event.get():
