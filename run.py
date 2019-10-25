329bac1bf78a7b64e3dfe2d32d8cd03bd5407e2f
Pranavi Boyalakuntla <naviatolin@gmail.com>
2019-10-25 14:33:57 -0400

    Snapping and offset

--- run.py
+++ run.py
@@ -2,14 +2,18 @@ import pygame, sys
 from pygame.locals import *
 from classes import Screen, Player, Tile, Board, Piece
 
-def start_game(player1, player2, player1color, player2color):
-    screen = Screen(600, 60)
+def start_game(player1, player2, player3, player4, player1color, player2color, player3color, player4color):
+    screen = Screen(1500, 900, 90)
     screen.start_board_display()
 
     player1 = Player(player1, player1color)
     player1.initialize_pieces(screen)
     player2 = Player(player2, player2color)
     player2.initialize_pieces(screen)
+    player3 = Player(player3, player3color)
+    player3.initialize_pieces(screen)
+    player4 = Player(player4, player4color)
+    player4.initialize_pieces(screen)
     
     board = Board(screen)
     # board.draw()
@@ -18,11 +22,12 @@ def start_game(player1, player2, player1color, player2color):
     while running:
         board.draw()
         player1.L5.draw()
-        # print(player1.L5.piece_array)
-        # board.draw()
-        pygame.display.flip()       
+        print(pygame.mouse.get_pressed()[0])
+        print(player1.L5.is_used)
+        screen.display_update(board.board_rectangle)
+        pygame.display.update(board.board_rectangle)       
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-start_game("shawn","navi","blue","red")
+start_game("shawn","navi", "ben", "hill", "blue","red", "green", "yellow")
