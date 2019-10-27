a710bedba1098a7936d3d4fd73829036b9c3d99c
Shawn Albertson <shawn.albertson@students.olin.edu>
2019-10-27 17:40:22 -0400

    Include Player class, collision detection, piece definitions, turn interaction

--- model.py
+++ model.py
@@ -2,12 +2,53 @@ import pygame
 
 # Functions to load images
 def piece_size(board_size, n):
-    """Take board dimension tuple and number of desired pieces per board
+    """Take board dimension and number of desired pieces per board
     Return the length of one edge of a tile as int type for pygame.transform.scale
 
     A regular Blokus board is 20x20
     """
-    return int(min(board_size)/n)
+    # if a tuple:
+    # return int(min(board_size)/n)
+    # if a single number
+    return int(board_size/n)
+
+
+
+class Screen:
+    def __init__(self, screen_size, fps):
+        """
+        Initialize the screen dimension, and fps. Set the board's screen size.
+        
+        Arguments:
+            board_screen_dimension : integer
+                Number that is the height and width of the board screen
+
+            fps : integer
+                Frames per second
+        """
+        self.screen_size = screen_size
+        self.fps = fps
+        self.board_screen = pygame.display.set_mode((self.screen_size, self.screen_size))
+
+    def start_board_display(self):
+        """
+        Start the board display - only called once at the beginning of the game. Sets the program clock.
+        """
+        # Initialize pygame interface
+        pygame.init()
+
+        # Avoid copyright issues with convenient typos
+        pygame.display.set_caption('Blockus')
+        
+        self.board_screen.fill((177,177,177))
+        self.clock = pygame.time.Clock()
+
+    def display_update(self, rect):
+        """
+        Updates the display by the fps set.
+        """
+        pygame.display.update(rect)
+        self.clock.tick(self.fps)
 
 class Tile:
     """Includes color and size of tiles
@@ -47,10 +88,11 @@ class Board:
     get_mouse determines if the image will follow your mouse or not
     """
 
-    def __init__(self, Screen, get_mouse = True):
+    def __init__(self, screen, tiles, array, get_mouse = True):
 
         self.screen = screen
         self.size = tiles.size
+        self.tiles = tiles
 
         # Make color images
         self.white = tiles.tile1.image
@@ -59,7 +101,18 @@ class Board:
         self.yellow = tiles.tile4.image
         self.blue = tiles.tile5.image
 
-        self.array = array
+        if array == "board":
+            board = []
+            row = []
+            for m in range(20):
+                for n in range(20):
+                    row.append("w")
+                board.append(row)
+                row = []
+            self.array = board
+        else:
+            self.array = array
+            
         self.get_mouse = get_mouse
 
     def draw(self):
@@ -70,22 +123,23 @@ class Board:
             (start_x, start_y) = pygame.mouse.get_pos()
         else:
             (start_x, start_y) = (0,0)
-
         for i1, m in enumerate(self.array):
             for i2, n in enumerate(m):
                 if n == "w":
-                    self.screen.blit(self.white, (self.size*i1 + start_x, self.size*i2 + start_y))
+                    self.screen.blit(self.white, (self.size*i2 + start_x, self.size*i1 + start_y))
                 elif n == "g":
-                    self.screen.blit(self.green, (self.size*i1 + start_x, self.size*i2 + start_y))
+                    self.screen.blit(self.green, (self.size*i2 + start_x, self.size*i1 + start_y))
                 elif n == "r":
-                    self.red, (self.size*i1 + start_x, self.size*i2 + start_y))
+                    self.screen.blit(self.red, (self.size*i2 + start_x, self.size*i1 + start_y))
                 elif n == "y":
-                    self.screen.blit(self.yellow, (self.size*i1 + start_x, self.size*i2 + start_y))
+                    self.screen.blit(self.yellow, (self.size*i2 + start_x, self.size*i1 + start_y))
                 elif n == "b":
-                    self.screen.blit(self.blue, (self.size*i1 + start_x, self.size*i2 + start_y))                                                         
+                    self.screen.blit(self.blue, (self.size*i2 + start_x, self.size*i1 + start_y))                                                         
                 else:
                     continue    
     
+
+
     def modify_board(self, modifier, start):
         """Rewrite certain chunks of array to a new color
 
@@ -101,20 +155,44 @@ class Board:
 
         This is a good place to check that the piece fits on the board in the first place
         """
-        for i1, m in enumerate(modifier.array):
-            for i2, n in enumerate(m):
-                check = self.array[i1 + start[0]][i2 + start[1]]
-                if not n == "." and check == "w":
-                    self.array[i1 + start[0]][i2 + start[1]] = n
-                elif not check == "w":
-                    # raise NameError('No')
-                else:
-                    continue
+        # A list to fill with the tiles that have already been drawn on 
+        current_write= []
+
+        for row, m in enumerate(modifier.array):
+            current_write.append([])
+            for column, n in enumerate(m):
+                check = self.array[row + start[1]][column + start[0]]
+                
+                # The case where the piece array is getting drawn to the board array
+                if n != "." and check == "w":
+                    self.array[row + start[1]][column + start[0]] = n
+
+                    current_write[row].append(check)
+
+                # A blank in the piece array
+                elif n == ".":
+                    current_write[row].append(check)
+
+                # The case where a collision is detected
+                elif n != "." and check != "w":
+                    self.reset = Piece(self.screen, self.tiles, current_write, 1)
+                    self.reset_play(self.reset, start)
+                    return
+
+    def reset_play(self, modifier, start):
+        """
+            If a play is invalid, this changes the board back to where it would have been before the play
+        """
+        for row, m in enumerate(modifier.array):
+            for column, n in enumerate(m):
+                self.array[row + start[1]][column + start[0]] = n
+
 
 class Piece(Board):
 
-    def __init__(self, screen, tiles, array, get_mouse = True):
+    def __init__(self, screen, tiles, array, available, get_mouse = True):
         super().__init__(screen, tiles, array, get_mouse)
+        self.available = available
 
     def rotate_piece(self):
         """Modify array such that the piece rotates"""
@@ -124,8 +202,111 @@ class Piece(Board):
         """Take in a general array representing a piece
         Assign the general fill with the newly definied fill
         """
-        for i1, m in enumerate(self.array):
-            for i2, n in enumerate(m):
+
+        #THIS IS CORRECT FOR COLOR ASSIGNMENT
+        for row, m in enumerate(self.array):
+            for column, n in enumerate(m):
                 if not n == ".":
-                    self.array[i1][i2] = color
+                    self.array[row][column] = color
     
+
+
+class Player:
+    def __init__(self, player_name, player_color):
+        """
+        Initialize the player with what color their pieces should be and what their name is.
+
+        Arguments:
+            player_name : string
+                Name of the player
+
+            player_color : string
+                Color that the pieces of the player are going to be
+        """
+
+        self.player_name = player_name
+        self.player_color = player_color
+
+        if self.player_color == "red":
+            self.click_color = "r"
+        if self.player_color == "blue":
+            self.click_color = "b"
+        if self.player_color == "green":
+            self.click_color = "g"
+        if self.player_color == "yellow":
+            self.click_color = "y"
+            
+    def initialize_pieces(self, Screen, Tiles, available = 1):
+        """
+        Initializes all pieces objects for a player.
+        
+        Arguments:
+            Screen : Pygame object
+                Pygame screen object
+        """
+        self.available = available
+
+        # create all of the pieces and assign a used or not used to them
+        self.V5 = Piece(Screen, Tiles, [["x", "x", "x"], ["x",".","."], ["x",".","."]], self.available)
+        self.V5.assign_color(self.click_color)
+
+        self.V3 = Piece(Screen, Tiles, [["x", "x"], ["x","."]], self.available)
+        self.V3.assign_color(self.click_color)        
+
+        self.X = Piece(Screen, Tiles, [[".", "x", "."], ["x", "x", "x"], [".", "x", "."]], self.available)
+        self.X.assign_color(self.click_color)
+        
+        self.N = Piece(Screen, Tiles, [[".", ".", "x", "x"], ["x", "x", "x", "."]], self.available)
+        self.N.assign_color(self.click_color)
+
+        self.L5 = Piece(Screen, Tiles, [["x", "x", "x", "x"], [".", ".", ".", "x"]], self.available)
+        self.L5.assign_color(self.click_color)
+
+        self.L4 = Piece(Screen, Tiles, [["x", "x", "x"], [".", ".", "x"]], self.available)
+        self.L4.assign_color(self.click_color)
+
+        self.Y = Piece(Screen, Tiles, [["x", "x", "x", "x"], [".", "x", ".", "."]], self.available)
+        self.Y.assign_color(self.click_color)
+
+        self.U = Piece(Screen, Tiles, [["x", ".", "x"], ["x", "x", "x"]], self.available)
+        self.U.assign_color(self.click_color)  
+
+        self.P = Piece(Screen, Tiles, [["x", "x", "."], ["x", "x", "x"]], self.available)
+        self.P.assign_color(self.click_color)
+
+        self.W = Piece(Screen, Tiles, [["x", "x", "."], [".", "x", "x"], [".", ".", "x"]], self.available)
+        self.W.assign_color(self.click_color)
+
+        self.F = Piece(Screen, Tiles, [["x", "x", "."], [".", "x", "x"], [".", "x", "."]], self.available)
+        self.F.assign_color(self.click_color)      
+
+        self.T5 = Piece(Screen, Tiles, [["x", "x", "x"], [".", "x", "."], [".", "x", "."]], self.available)
+        self.T5.assign_color(self.click_color)          
+
+        self.T4 = Piece(Screen, Tiles, [["x", "x", "x"], [".", "x", "."]], self.available)
+        self.T4.assign_color(self.click_color)
+
+        self.Z5 = Piece(Screen, Tiles, [["x", "x", "."], [".", "x", "."], [".", "x", "x"]], self.available)
+        self.Z5.assign_color(self.click_color)
+
+        self.Z4 = Piece(Screen, Tiles, [["x", "x", "."], [".", "x", "x"]], self.available)
+        self.Z4.assign_color(self.click_color)
+
+        self.S5 = Piece(Screen, Tiles, [["x", "x", "x", "x", "x"]], self.available)
+        self.S5.assign_color(self.click_color)
+
+        self.S4 = Piece(Screen, Tiles, [["x", "x", "x", "x"]], self.available)
+        self.S4.assign_color(self.click_color)
+
+        self.S3 = Piece(Screen, Tiles, [["x", "x", "x"]], self.available)
+        self.S3.assign_color(self.click_color)
+
+        self.S2 = Piece(Screen, Tiles, [["x", "x"]], self.available)
+        self.S2.assign_color(self.click_color)
+
+        self.S1 = Piece(Screen, Tiles, [["x"]], self.available)
+        self.S1.assign_color(self.click_color)
+
+        self.square = Piece(Screen, Tiles, [["x", "x"], ["x", "x"]], self.available)
+        self.square.assign_color(self.click_color)
+        
--- shawn.py
+++ shawn.py
@@ -1,91 +1,254 @@
 import pygame
 import time
 
-from model import piece_size, Tile, Tiles, Board, Piece
+from model import *
+
+def load(screen_dim, screen_fps, f1, f2, f3, f4, f5):
+    global screen
+    global size
+    global white
+    global green
+    global red
+    global yellow
+    global blue
+    global tiles
+    global board
+
+    # Initialize screen
+    screen = Screen(screen_dim, screen_fps)
+    
+    # Initialize size of one piece
+    size = piece_size(screen.screen_size, 20)
+
+    # Load image files as Tile objects
+    white = Tile(f1, size, ".")
+    green = Tile(f2, size, "g")
+    red = Tile(f3, size, "r")
+    yellow = Tile(f4, size, "y")
+    blue = Tile(f5, size, "b")
+
+    tiles = Tiles(white, green, red, yellow, blue)
+
+    board = Board(screen.board_screen, tiles, "board", False)
+
+def place_piece(player, board, piece):
+
+    if piece.available:
+        draw = True
+
+        while draw:
+            board.draw()
+            piece.draw()
+
+# Now go through the logic of detecting a click and placing the piece accordingly
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
 
 
-# Initialize pygame, screen object
-pygame.init()
+                if pygame.mouse.get_pressed()[0]:
+                    # snap to the integer position
+                    mouse_x, mouse_y = event.pos
+                    mouse_x = int(mouse_x/size)
+                    mouse_y = int(mouse_y/size)
 
-board_size = (700, 700)
-screen = pygame.display.set_mode(board_size)
+# Modify_board does the work of changing the board array                       
+                    board.modify_board(piece, (mouse_x, mouse_y)) 
+                    draw = False
 
-# Avoid copyright issues with convenient typos
-pygame.display.set_caption('Blockus')
+                pygame.display.flip()
 
-# This gray color fill is meaningless once pieces start getting placed
-background_color = (177,177,177)
-screen.fill(background_color)
-
-# Define the size of one tile
-size = piece_size(board_size, 20)
+    else:
+# If the piece has been played, call your_turn again so you don't go on the next player yet
+        print("You already played this piece!")
+        your_turn(player, board)
+    
 
 
-# Make the necessary Tile objects
-white = Tile("white.png", size, ".")
-green = Tile("green.png", size, "g")
-red = Tile("red.png", size, "r")
-yellow = Tile("yellow.png", size, "y")
-blue = Tile("blue.png", size, "b")
-
-# Add them to a Tiles object to be concise
-tiles = Tiles(white, green, red, yellow, blue)
-
-# Call board_array() to make the base board
-def board_array():
-    board = []
-    row = []
-    for m in range(20):
-        for n in range(20):
-            row.append("w")
-        board.append(row)
-        row = []
-    return board
-
-board_array = board_array()
-
-# Create board object from board_array
-b = Board(screen, tiles, board_array, False)
-
-# Here are a few arrays that define pieces
-L5_array = [["x", "x", "x"], ["x",".","."], ["x",".","."]]
-plus_array = [[".", "x", "."], ["x", "x", "x"], [".", "x", "."]]
-N_array = [[".", ".", "x", "x"], ["x", "x", "x", "."]]
-blank_array = [["."]]
-
-# Here we create Piece objects using these arrays
-L5 = Piece(screen, tiles, L5_array)
-plus = Piece(screen, tiles, plus_array)
-N = Piece(screen, tiles, N_array)
-blank = Piece(screen, tiles, blank_array)
-
-# Assign colors to the pieces
-L5.assign_color("r")
-plus.assign_color("b")
-N.assign_color("y")
-
-def play_game(board, piece, draw = True):
-    while draw:
+def your_turn(player, board):
+    # Establish a running status called 'running'
+    running = True
+    while running:
+    # Draw the board first to establish the "waiting" period before a piece is selected
         board.draw()
-        piece.draw()
+        pygame.display.flip()
+    
+    # Give the option to close the window in this phase
         for event in pygame.event.get():
-
-            if pygame.mouse.get_pressed()[0]:
-                # snap to the integer position
-                mouse_x, mouse_y = event.pos
-                mouse_x = int(mouse_x/size)
-                mouse_y = int(mouse_y/size)
-                
-                board.modify_board(piece, (mouse_x, mouse_y)) 
-                draw = False
-            pygame.display.flip()
-
+            # First layer to choose a piece
             if event.type == pygame.QUIT:
-                draw = False
+                pygame.quit()
+
+    # Once keyboard input is detected, enter a new part of the code with running status called 'draw'
+            if event.type == pygame.KEYDOWN:
+
+                if event.key == pygame.K_q:
+                    piece = player.V5
+                    place_piece(player, board, piece)
+                    piece.available = 0
+                    running = False
+
+                if event.key == pygame.K_w:
+                    piece = player.V3
+                    place_piece(player, board, piece)
+                    piece.available = 0
+                    running = False
+
+                if event.key == pygame.K_e:
+                    piece = player.X
+                    place_piece(player, board, piece)
+                    piece.available = 0
+                    running = False
+                
+                if event.key == pygame.K_r:
+                    piece = player.N
+                    place_piece(player, board, piece)
+                    piece.available = 0
+                    running = False
+
+                if event.key == pygame.K_t:
+                    piece = player.L5
+                    place_piece(player, board, piece)
+                    piece.available = 0
+                    running = False
+                                    
+                if event.key == pygame.K_i:
+                    piece = player.L4                    
+                    place_piece(player, board, piece)
+                    piece.available = 0
+                    running = False
+
+                if event.key == pygame.K_y:
+                    piece = player.Y
+                    place_piece(player, board, piece)
+                    piece.available = 0
+                    running = False
+
+                if event.key == pygame.K_u:
+                    piece = player.U
+                    place_piece(player, board, piece)
+                    piece.available = 0
+                    running = False
+
+                if event.key == pygame.K_p:
+                    piece = player.P
+                    place_piece(player, board, piece)
+                    piece.available = 0
+                    running = False
+
+                if event.key == pygame.K_o:
+                    piece = player.W         
+                    place_piece(player, board, piece)
+                    piece.available = 0
+                    running = False
+
+                if event.key == pygame.K_f:
+                    piece = player.F                               
+                    place_piece(player, board, piece)
+                    piece.available = 0
+                    running = False
+
+                if event.key == pygame.K_s:
+                    piece = player.T5
+                    place_piece(player, board, piece)
+                    piece.available = 0
+                    running = False
+
+                if event.key == pygame.K_d:
+                    piece = player.T4
+                    place_piece(player, board, piece)
+                    piece.available = 0
+                    running = False
+
+                if event.key == pygame.K_a:
+                    piece = player.Z5
+                    place_piece(player, board, piece)
+                    piece.available = 0
+                    running = False
+
+                if event.key == pygame.K_g:
+                    piece = player.Z4
+                    place_piece(player, board, piece)
+                    piece.available = 0
+                    running = False
+
+                if event.key == pygame.K_h:
+                    piece = player.S5
+                    place_piece(player, board, piece)
+                    piece.available = 0
+                    running = False
+
+                if event.key == pygame.K_j:
+                    piece = player.S4
+                    place_piece(player, board, piece)
+                    piece.available = 0
+                    running = False
+
+                if event.key == pygame.K_k:
+                    piece = player.S3
+                    place_piece(player, board, piece)
+                    piece.available = 0
+                    running = False
+
+                if event.key == pygame.K_l:
+                    piece = player.S2                                                                    
+                    place_piece(player, board, piece)
+                    piece.available = 0
+                    running = False
+
+                if event.key == pygame.K_z:
+                    piece = player.S1
+                    place_piece(player, board, piece)
+                    piece.available = 0
+                    running = False
+
+                if event.key == pygame.K_x:
+                    piece = player.square
+                    place_piece(player, board, piece)
+                    piece.available = 0
+                    running = False
 
 
 
-play_game(b, L5)
-play_game(b, blank)
-play_game(b, plus)
-play_game(b, blank)
+def play(p1, p2, p3, p4, p1color, p2color, p3color, p4color):
+    """
+        Initialize players and pieces
+    """
+
+    p1 = Player(p1, p1color)
+    p1.initialize_pieces(screen.board_screen, tiles)
+    p2 = Player(p2, p2color)
+    p2.initialize_pieces(screen.board_screen, tiles)
+    p3 = Player(p3, p3color)
+    p3.initialize_pieces(screen.board_screen, tiles)
+    p4 = Player(p4, p4color)
+    p4.initialize_pieces(screen.board_screen, tiles)
+
+
+    turn_counter = 0
+    # start by choosing your piece
+
+    while True:
+        if turn_counter == 0:
+
+            your_turn(p1, board)
+            turn_counter += 1
+
+        if turn_counter == 1:
+
+            your_turn(p2, board)
+            turn_counter += 1
+
+        if turn_counter == 2:
+
+            your_turn(p3, board)
+            turn_counter += 1
+
+        if turn_counter == 3:
+
+            your_turn(p4, board)    
+            turn_counter = 0
+
+load(600, 60, "white.png", "green.png", "red.png", "yellow.png", "blue.png")
+
+play("shawn", "navi", "ben", "hill", "blue", "green", "red", "yellow")
